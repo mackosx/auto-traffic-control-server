ab94dd949dcef3b1ca50caa5219adb88ed066c6d
Mackenzie Salloum <mackenziesalloum@gmail.com>
2022-11-13 13:19:48 -0800

    Avoid sharp turns

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,20 @@ import {
   UpdateFlightPlanRequest,
   Map,
   Airplane,
+  NodeToPointRequest,
+  NodeToPointResponse,
+  GetMapResponse,
 } from "auto-traffic-control";
-import { range } from "./util";
+import { Direction, direction, range, unitMove } from "./util";
+import { promisify } from "util";
+
+const eventService = new EventServiceClient("localhost:4747", getCredentials());
+const mapService = new MapServiceClient("localhost:4747", getCredentials());
+const airplaneService = new AirplaneServiceClient(
+  "localhost:4747",
+  getCredentials()
+);
+const gameService = new GameServiceClient("localhost:4747", getCredentials());
 
 /**
  * Converts a set of coordinates into the routing grid node.
@@ -26,31 +38,42 @@ import { range } from "./util";
  * @returns A reference to the node at the given long/lat
  */
 function getNodeAt(map: Map, longitude: number, latitude: number): Node {
-  const x = longitude + map.getWidth() / 2;
-  const y = latitude + map.getHeight() / 2;
+  const x = longitude + (map.getWidth() - 1) / 2;
+  const y = latitude + (map.getHeight() - 1) / 2;
   const index = y * map.getWidth() + x;
   return map.getRoutingGridList()[index];
 }
 
+const filterNeighbourInDirection =
+  (inDirection: Direction, origin: Node) => (node: Node) => {
+    const newCoords = unitMove[inDirection];
+    return !(
+      origin.getLongitude() + newCoords[0] === node.getLongitude() &&
+      origin.getLatitude() + newCoords[1] === node.getLatitude()
+    );
+  };
+
 /**
+ * The neighbours are a 3 by 3 box surrounding the current node.
  *
  * @param node Node we are getting the neighbours of
  * @param map Map to look for nodes in
  * @returns A list of all nodes surrounding the current node.
  */
 function getNeighbours(node: Node, map: Map): Node[] {
-  console.log(map.getHeight(), map.getWidth());
-  const width_range = range(
+  // Origin is at the middle of the map so subtract 1 divide by 2 gives
+  // us the range of either side of the origin
+  const widthRange = range(
     Math.min((map.getWidth() - 1) / 2, node.getLongitude() + 1),
     Math.max(-1 * ((map.getWidth() - 1) / 2), node.getLongitude() - 1)
   );
-  const height_range = range(
+  const heightRange = range(
     Math.min((map.getHeight() - 1) / 2, node.getLatitude() + 1),
     Math.max(-1 * ((map.getHeight() - 1) / 2), node.getLatitude() - 1)
   );
   const neighbours: Node[] = [];
-  height_range.forEach((y) => {
-    width_range.forEach((x) => {
+  heightRange.forEach((y) => {
+    widthRange.forEach((x) => {
       if (x !== node.getLongitude() && y !== node.getLatitude()) {
         neighbours.push(getNodeAt(map, x, y));
       }
@@ -59,14 +82,62 @@ function getNeighbours(node: Node, map: Map): Node[] {
   return neighbours;
 }
 
-function generateFlightPlan(
-  id: string,
-  start: Node,
+/**
+ * Airplanes are not allowed to immediately turn backwards,
+ * so we need to know what node they came from so we can
+ * filter out that node in our list of valid neighbours.
+ *
+ * First, convert the airplane's next node into a point.
+ *
+ * Compare that against the airplanes current location to
+ * determine the direction of flight,
+ *
+ * Once we know the direction, eliminate the node in the reverse of that direction.
+ * Example of valid next nodes from a given node with a northern direction (^):
+ * ```
+ *    * * *
+ *    * ^ *
+ *    * x *
+ * ```
+ */
+async function generateFlightPlan(
+  airplane: Airplane,
   destAirport: Airport,
   map: Map
-): Node[] {
+): Promise<Node[]> {
+  const flightPlan = airplane.getFlightPlanList();
+  const next = flightPlan[0];
+  if (!next) {
+    console.error("No node in the flight plan to start from...");
+    return flightPlan;
+  }
+  // @ts-ignore
+  mapService.nodeToPointPromise = promisify<
+    NodeToPointRequest,
+    NodeToPointResponse
+  >(mapService.nodeToPoint);
+  // @ts-ignore
+  const response = await mapService.nodeToPointPromise(
+    new NodeToPointRequest().setNode(next)
+  );
+  const point = response.getPoint();
+  if (!point) {
+    console.error("Can't determine point of node: " + next);
+    return flightPlan;
+  }
+  const currentAirplanePoint = airplane.getPoint();
+  if (!currentAirplanePoint) {
+    console.error("Can't determine point of airplane: " + airplane);
+    return flightPlan;
+  }
+  const planeDirection = direction(currentAirplanePoint, point);
   // Testing with sending any nodes
-  return getNeighbours(start, map).filter((node) => !node.getRestricted());
+  const filterFromNode = filterNeighbourInDirection(planeDirection, next);
+  const possibleNodes = getNeighbours(next, map)
+    .filter((node) => !node.getRestricted())
+    .filter(filterFromNode);
+  console.log("Possible next moves: " + possibleNodes);
+  return [next, possibleNodes[0]];
 }
 
 function getAirportForAirplane(map: Map, airplane: Airplane): Airport {
@@ -81,101 +152,80 @@ function getAirportForAirplane(map: Map, airplane: Airplane): Airport {
   return matchingAirport;
 }
 
-function updateFlightPlan(
-  event: AirplaneDetected,
-  mapService: MapServiceClient,
-  airplaneService: AirplaneServiceClient
-) {
+async function updateFlightPlan(event: AirplaneDetected) {
   const airplane = event.getAirplane();
   if (airplane == undefined) {
     throw new Error("Received AirplaneDetected event without an airplane");
   }
-
-  const id = airplane.getId();
-  const flightPlan = airplane.getFlightPlanList();
-  const nextNode = flightPlan[0];
-  if (!nextNode) {
+  const next = airplane.getFlightPlanList()[0];
+  // @ts-ignore
+  mapService.getMapPromise = promisify<GetMapRequest, GetMapResponse>(
+    mapService.getMap
+  );
+  // @ts-ignore
+  const response = await mapService.getMapPromise(new GetMapRequest());
+  const map = response.getMap();
+  if (!map) {
+    console.error("No map...");
     return;
   }
+  const airport = getAirportForAirplane(map, airplane);
+  const newFlightPlan = await generateFlightPlan(airplane, airport, map);
+  console.log("New flight plan acquired " + newFlightPlan + " for " + airplane);
 
-  mapService.getMap(new GetMapRequest(), (err, response) => {
-    if (err != null) {
-      throw err;
-    }
-    const map = response.getMap();
-    if (!map) {
-      console.error("No map...");
-      return;
-    }
-    const airport = getAirportForAirplane(map, airplane);
-    const newFlightPlan = generateFlightPlan(id, nextNode, airport, map);
-    airplaneService.updateFlightPlan(
-      new UpdateFlightPlanRequest()
-        .setId(airplane.getId())
-        .setFlightPlanList(newFlightPlan),
-      (err, response) => {
-        if (err != null) {
-          throw err;
-        }
-        // Log any validation errors with the flight plan
-        const error = response.getError();
-        if (error) {
-          const errorsList = error.getErrorsList();
-          console.log("Flight plan invalid. Errors: " + errorsList);
-          return;
-        }
-        console.log("Updated " + airplane + "'s flight plan.");
+  airplaneService.updateFlightPlan(
+    new UpdateFlightPlanRequest()
+      .setId(airplane.getId())
+      .setFlightPlanList(newFlightPlan),
+    (err, response) => {
+      if (err != null) {
+        throw err;
       }
-    );
-  });
-
-  console.log(`Detected airplane ${id} heading towards ${nextNode}.`);
+      // Log any validation errors with the flight plan
+      const error = response.getError();
+      if (error) {
+        const errorsList = error.getErrorsList();
+        console.log("Flight plan invalid. Errors: " + errorsList);
+        return;
+      }
+      console.log("Updated " + airplane + "'s flight plan.");
+    }
+  );
+  console.log(`Detected airplane ${airplane.getId()} heading towards ${next}.`);
 
   return;
 }
+
 function exit(event: GameStopped): void {
   const score = event.getScore();
 
   console.log(`Game stopped! Score: ${score}`);
   process.exit();
 }
-const processMessage =
-  (mapService: MapServiceClient, airplaneService: AirplaneServiceClient) =>
-  (streamResponse: StreamResponse) => {
-    const airplaneDetected = streamResponse.getAirplaneDetected();
-    if (airplaneDetected != undefined) {
-      updateFlightPlan(airplaneDetected, mapService, airplaneService);
-    }
 
-    const gameStopped = streamResponse.getGameStopped();
-    if (gameStopped != undefined) {
-      exit(gameStopped);
-    }
-  };
+function processMessage(streamResponse: StreamResponse) {
+  const airplaneDetected = streamResponse.getAirplaneDetected();
+  if (airplaneDetected != undefined) {
+    updateFlightPlan(airplaneDetected);
+  }
+
+  const gameStopped = streamResponse.getGameStopped();
+  if (gameStopped != undefined) {
+    exit(gameStopped);
+  }
+}
 
 function streamClosed() {
   console.log("Event stream closed.");
 }
 
 function subscribeToEvents(): void {
-  const eventService = new EventServiceClient(
-    "localhost:4747",
-    getCredentials()
-  );
-  const mapService = new MapServiceClient("localhost:4747", getCredentials());
-  const airplaneService = new AirplaneServiceClient(
-    "localhost:4747",
-    getCredentials()
-  );
-
   const stream = eventService.stream(new StreamRequest());
-  stream.on("data", processMessage(mapService, airplaneService));
+  stream.on("data", processMessage);
   stream.on("end", streamClosed);
 }
 
 function startGame() {
-  const gameService = new GameServiceClient("localhost:4747", getCredentials());
-
   gameService.startGame(new StartGameRequest(), (err) => {
     if (err != null) {
       throw err;
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import { Point } from "auto-traffic-control";
+
 /**
  *
  * @param stop Last value (inclusive) of the array
@@ -7,4 +9,64 @@
 export function range(stop: number, start: number) {
   const size = Math.abs(start - stop) + 1;
   return [...Array(size).keys()].map((i) => i + start);
+}
+
+export enum Direction {
+  NORTH = "N",
+  EAST = "E",
+  SOUTH = "S",
+  WEST = "W",
+  NORTH_EAST = "NE",
+  NORTH_WEST = "NW",
+  SOUTH_EAST = "SE",
+  SOUTH_WEST = "SW",
+}
+
+export const unitMove = {
+  [Direction.EAST]: [1, 0],
+  [Direction.WEST]: [-1, 0],
+  [Direction.NORTH]: [0, 1],
+  [Direction.SOUTH]: [0, -1],
+  [Direction.NORTH_EAST]: [1, 1],
+  [Direction.SOUTH_EAST]: [1, -1],
+  [Direction.NORTH_WEST]: [-1, 1],
+  [Direction.SOUTH_WEST]: [-1, -1],
+};
+
+export function direction(from: Point, to: Point): Direction {
+  const x = from.getX() - to.getX();
+  const y = from.getY() - to.getY();
+
+  if (x == 0) {
+    if (y <= 0) {
+      return Direction.SOUTH;
+    }
+    if (y > 0) {
+      return Direction.NORTH;
+    }
+  }
+  if (x < 0) {
+    if (y == 0) {
+      return Direction.WEST;
+    }
+    if (y < 0) {
+      return Direction.SOUTH_WEST;
+    }
+    if (y > 0) {
+      return Direction.NORTH_WEST;
+    }
+  }
+  if (x > 0) {
+    if (y == 0) {
+      return Direction.EAST;
+    }
+    if (y < 0) {
+      return Direction.SOUTH_EAST;
+    }
+    if (y > 0) {
+      return Direction.NORTH_EAST;
+    }
+  }
+  // Default to get nice typing
+  return Direction.EAST;
 }
